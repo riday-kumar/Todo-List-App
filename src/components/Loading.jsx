f2c0d6edc80118561f93964d837c21b8d6f9abb3
riday-kumar <hridoykumarsaha07@gmail.com>
2026-04-30 12:52:48 +0600

    loading component update

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,8 +3,11 @@ import React from "react";
 const Loading = () => {
   return (
     <div className="flex gap-3 justify-center items-center h-dvh bg-primary text-white">
-      <p className="text-5xl font-bold">Loading </p>
-      <span className="loading loading-spinner text-warning text-5xl"></span>
+      <p className="text-5xl font-bold">
+        L
+        <span className="loading loading-spinner text-warning text-5xl mx-3"></span>
+        ading{" "}
+      </p>
     </div>
   );
 };
